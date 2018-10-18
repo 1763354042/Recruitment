1376e6148912c9b06d0ad8944159dd6902f33590
ergouzi <1763354042@qq.com>
2018-10-18 22:59:29 +0800

    添加代理测试，修改大街网

--- pc20/daJie.py
+++ pc20/daJie.py
@@ -1,20 +1,42 @@
 import requests
 import json
-from mongoSitting import *
+from mongoSitting import sava_to_mongo
+from multiprocessing import Pool
 
-keyWord = ["C++","python","java"]
-def open_sitting_sitting(i):
-    with open('sitting.json','r') as f:
-        json_result = json.load(f)
-        return json_result['user-agent'][i]
+def get_keyWord(i):
+    with open('sitting.json','r',encoding='utf-8') as load_f:
+        keyWord = json.load(load_f)["keyWord"]
+        lens = len(keyWord)
+        print(lens)
+        keyWord = keyWord[i%lens]
+    return keyWord
+
+PROXY_POOL_URL = 'http://127.0.0.1:5555/random'
+def get_proxy():
+    try:
+        response = requests.get(PROXY_POOL_URL)
+        if response.status_code == 200:
+            return response.text
+    except ConnectionError:
+        return None
+
 
 def main(i):
-    headers = {
-        'user-agent':open_sitting_sitting(i),
+    keyWord= get_keyWord(i)
+    proxy = get_proxy()
+    proxies = {
+        'http':'http://'+proxy,
+        'https':'https://'+proxy
     }
-    url = 'https://so.dajie.com/job/ajax/search/filter?keyword='+keyWord[i%3]+'&order=0&city=&recruitType=&salary=&experience=&page='+str(i)+'&positionFunction=&_CSRFToken=&ajax=1'
-    res = requests.get(url,headers=headers)
-    print(res.text)
+    firstUrl = 'https://so.dajie.com/job/search'
+    url = 'https://so.dajie.com/job/ajax/search/filter?keyword='+keyWord+'&order=0&city=&recruitType=&salary=&experience=&page='+str(i+1)+'&positionFunction=&_CSRFToken=ZHc07e-ibW9qugRFMAllwA6MvdUiieGIdXGM4w1h&ajax=1'
+    session = requests.session()
+    session.get(firstUrl)
+    session.headers['referer'] = firstUrl
+   # session.headers['proxies'] = proxies
+    responose = session.get(url)
+    print(responose.text)
 
 if __name__ == '__main__':
-    main(1)
+    pool = Pool()
+    pool.map(main, [i*1 for i in range(10)])
--- pc20/liePin.py
+++ pc20/liePin.py
@@ -8,26 +8,28 @@ def string_handle(soup,keyWord):
     ul = soup.find(attrs={'class': 'sojob-list'})
     lis = ul.find_all('li')
     for li in lis:
-        a=li.find_all('a')
-        span = li.find_all('span')
-        position = {
-            'keyWord': keyWord,
-            'companyName':a[2].text.lstrip(),
-            'positionName':a[0].text.lstrip(),
-            'jobNature':'全职',
-            'workYear':span[2].text,
-            'education':span[1].text,                   #     test  出错******************************************
-            'city':a[1].text.lstrip(),
-            'salary':(float((span[0].text[:-1]).split('-')[0])+float((span[0].text[:-1]).split('-')[1]))/2
-        }
-        sava_to_mongo(position)
+        try:
+            a = li.find_all('a')
+            span = li.find_all('span')
+            position = {
+                'keyWord': keyWord,
+                'companyName': a[2].text.lstrip(),
+                'positionName': a[0].text.lstrip(),
+                'jobNature': '全职',
+                'workYear': span[2].text,
+                'education': span[1].text,  # test  出错******************************************
+                'city': a[1].text.lstrip(),
+                'salary': (float((span[0].text[:-1]).split('-')[0]) + float((span[0].text[:-1]).split('-')[1])) / 2
+            }
+            sava_to_mongo(position)
+        except:
+            continue
         print(position)
 
 
 
 def main(i):
     url = 'https://www.liepin.com/zhaopin/?ckid=f85a7e2cdb27186c&fromSearchBtn=2&init=-1&sfrom=click-pc_homepage-centre_searchbox-search_new&degradeFlag=0&key='+keyWord[i%3]+'&headckid=f85a7e2cdb27186c&d_pageSize=40&siTag=I-7rQ0e90mv8a37po7dV3Q%7EfA9rXquZc5IkJpXC-Ycixw&d_headId=6b34de8295422a575511f372881718fa&d_ckId=6b34de8295422a575511f372881718fa&d_sfrom=search_fp&d_curPage='+str(i+1)+'&curPage='+str(i)
-    #url = 'https://www.liepin.com/zhaopin/?ckid=5a1bf35f1bb140bb&fromSearchBtn=2&init=-1&sfrom=click-pc_homepage-centre_searchbox-search_new&flushckid=1&dqs=070020&key=前端&headckid=5a1bf35f1bb140bb&d_pageSize=40&siTag=I-7rQ0e90mv8a37po7dV3Q%7Eb82XZEfT3k9nLaGd2nO5lg&d_headId=dc30e93b15743c7e281185f2dcdcd55a&d_ckId=dc30e93b15743c7e281185f2dcdcd55a&d_sfrom=search_fp&d_curPage='+str(i+1)+'&curPage='+str(i)
     res = requests.get(url).text
     soup = bs(res, 'lxml')
     string_handle(soup,keyWord[i%3])
